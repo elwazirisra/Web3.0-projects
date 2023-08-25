f441c7f2c48dfd395ef508df73b82557f4663742
elwazirisra <elwazirisra2002@gmail.com>
2023-08-24 21:16:39 -0400

    push

---
+++ web3.0/client/src/components/Services.jsx
@@ -0,0 +1,19 @@
+import { BsShieldFillCheck } from "react-icons/bs";
+import { BiSearchAlt } from 'react-icons/bi';
+import { RiHeart2Fill } from 'react-icons/ri';
+
+
+const Services = () => {
+    return(
+        <div className="flex w-full justify-center items-center gradient-bg-services">
+          <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+              <div className="flex-1 fles flex-col justify-start items-start">
+                  <h1>Services that we</h1>
+
+              </div>
+
+          </div>
+        </div>
+    )
+}
+export default Services;
